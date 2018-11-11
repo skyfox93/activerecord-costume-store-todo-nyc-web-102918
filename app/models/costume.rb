a51fcc971f3ca1d81c29563346d4a4fed8735c06
Skylar Salerno <skylar.salerno@gmail.com>
2018-11-11 18:19:46 -0500

    Done.

--- app/models/costume.rb
+++ app/models/costume.rb
@@ -1,2 +1,16 @@
 # Create your Costume class here
 # It should inherit from ActiveRecord::Base
+class Costume < ActiveRecord::Base
+attr_accessor :created_at, :updated_at
+
+  def initialize(hash)
+    super
+    @created_at = DateTime.now
+  end
+
+  def update(val)
+    super(val)
+    @updated_at = DateTime.now
+  end
+
+end
